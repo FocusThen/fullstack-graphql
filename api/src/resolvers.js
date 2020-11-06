4cc4332129ccf6eff604d9f44ec8e36e0314df17
M. Mücahit TEZCAN <mmucahit.tezcan@gmail.com>
2020-11-06 13:27:22 +0300

    server side exercies done ✅

--- api/src/resolvers.js
+++ api/src/resolvers.js
@@ -5,19 +5,23 @@
 
 module.exports = {
   Query: {
-    
+    pets(_, { input }, ctx) {
+      return ctx.models.Pet.findMany(input)
+    },
+    pet(_, { input }, ctx) {
+      return ctx.models.Pet.findOne(input)
+    },
   },
   Mutation: {
-    
+    newPet(_, { input }, ctx) {
+      const pet = ctx.models.Pet.create(input)
+      return pet
+    },
   },
   Pet: {
-    img(pet) {
-      return pet.type === 'DOG'
-        ? 'https://placedog.net/300/300'
-        : 'http://placekitten.com/300/300'
-    }
+    owner(pet, _, ctx) {
+      return ctx.models.User.findOne(pet.user)
+    },
   },
-  User: {
-    
-  }
+  User: {},
 }
